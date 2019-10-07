cdc46c9e96b26c1f2427e0281462f27b82b818bb
Jan Kleinert <jan.kleinert@dlr.de>
2019-10-07 22:54:37 +0200

    added disfunctional topo sort

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,52 @@ pub trait SimpleGraph<'a>
         return res;
     }
 
+        /// get topologically sorted vector of nodes for a graph
+    fn get_topological_order(&self) -> Result<Vec::<&Self::N>, &'static str> {
+
+        // Kahn's method
+        let nodes = self.nodes();
+        let n = nodes.len();
+
+        // calculate in-degree
+        let mut in_degree = vec![0; n];
+        for i in 0..n {
+            in_degree[i] = self.ancestors(nodes[i]).len();
+        }
+
+        // initialize candidate list and return list
+        let mut candidates = Vec::<&Self::N>::new();
+        let mut list = Vec::<&Self::N>::new();
+
+        // candidates are all nodes with zero in-degree
+        for i in 0..n {
+            if in_degree[i] == 0 {
+                candidates.push(nodes[i]);
+            }
+        }
+
+        while candidates.len() > 0 {
+            let node = candidates.pop().unwrap();
+            list.push(node);
+
+            let children = self.children(node);
+            for child in children {
+                //TODO is there a faster way to get the index of the child?
+                let idx = nodes.iter().position(|&x| x==child).unwrap();
+                in_degree[idx] -= 1;
+                if in_degree[idx] == 0 {
+                    candidates.push(nodes[idx]);
+                }
+            }
+        }
+        if list.len() == n {
+            return Ok(list);
+        }
+        else {
+            return Err("Circular dependency detected!")
+        }
+    }
+
 }
 
 
