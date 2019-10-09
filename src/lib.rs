9a3ccc71a207ef01f857065b66e7fda75b18a707
Jan Kleinert <jan.kleinert@dlr.de>
2019-10-09 07:05:00 +0200

    use generic container rather than supertrait

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,13 @@ use std::hash::Hash;
 use std::collections::HashMap;
 
 /// A simple graph implementation for any collection that implements [`SuperIndex`] and [`IntoIterator`].
-pub trait SimpleGraph<'a, I : Eq + Hash + Copy> : SuperIndex<'a, I> + IntoIterator
+pub trait SimpleGraph<'a, I : Eq + Hash + Copy>
 {
+    type C : 'a + SuperIndex<'a, I> + IntoIterator;
+
+    /// gets a reference to the node container
+    fn nodes(&'a self) -> &Self::C;
+
     /// gets the indices of the children of a node with index `index`.
     fn children(&'a self, index: I) -> Vec<I>;
 
@@ -22,7 +27,7 @@ pub trait SimpleGraph<'a, I : Eq + Hash + Copy> : SuperIndex<'a, I> + IntoIterat
     //TODO can this be cached??
     fn ancestors(&'a self, i: I) -> Vec<I> {
         let mut res = Vec::<I>::new();
-        for (idx, _) in self.enumerate() {
+        for (idx, _) in self.nodes().enumerate() {
             for child_idx in self.children(idx) {
                 if child_idx == i {
                     res.push(idx);
@@ -39,7 +44,7 @@ pub trait SimpleGraph<'a, I : Eq + Hash + Copy> : SuperIndex<'a, I> + IntoIterat
 
         // calculate in-degree
         let mut in_degree = HashMap::<I, usize>::new(); // using a HashMap for usize keys maybe not ideal
-        for (i,_) in self.enumerate() {
+        for (i,_) in self.nodes().enumerate() {
             in_degree.insert(i, self.ancestors(i).len());
         }
         let n = in_degree.len();
@@ -49,7 +54,7 @@ pub trait SimpleGraph<'a, I : Eq + Hash + Copy> : SuperIndex<'a, I> + IntoIterat
         let mut list = Vec::<I>::new();
 
         // candidates are all nodes with zero in-degree
-        for (i,_) in self.enumerate() {
+        for (i,_) in self.nodes().enumerate() {
             if in_degree[&i] == 0 {
                 candidates.push(i);
             }
@@ -104,18 +109,24 @@ mod tests {
     use super::*;
 
     // create circular graph by directly implementing SimpleGraph on `Vec<usize>`
-    type UnidirectionalCircle = Vec<usize>;
+    struct UnidirectionalCircle(Vec<usize>);
 
-    impl<'a> SimpleGraph<'a, usize> for UnidirectionalCircle {
+    impl SimpleGraph<'_, usize> for UnidirectionalCircle {
+
+        type C = Vec<usize>;
+        
+        fn nodes(&self) -> &Self::C {
+            &self.0
+        }
 
         fn children(&self, i: usize) -> Vec::<usize> {
-            vec![(i+1) % self.len()]
+            vec![(i+1) % self.0.len()]
         }
     }
 
     #[test]
     fn trait_uni_circle_children_ok() {
-        let circle : UnidirectionalCircle = vec![0, 1, 2, 3, 4, 5];
+        let circle = UnidirectionalCircle(vec![0, 1, 2, 3, 4, 5]);
         assert_eq!(circle.children(0)[0], 1);
         assert_eq!(circle.children(1)[0], 2);
         assert_eq!(circle.children(2)[0], 3);
@@ -126,7 +137,7 @@ mod tests {
 
     #[test]
     fn trait_uni_circle_ancestors_ok() {
-        let circle : UnidirectionalCircle = vec![0, 1, 2, 3, 4, 5];
+        let circle = UnidirectionalCircle(vec![0, 1, 2, 3, 4, 5]);
         assert_eq!(circle.ancestors(0)[0], 5);
         assert_eq!(circle.ancestors(1)[0], 0);
         assert_eq!(circle.ancestors(2)[0], 1);
@@ -136,19 +147,25 @@ mod tests {
     }
 
     // create circular graph by directly implementing SimpleGraph on `Vec<i32>`
-    type BidirectionalCircle = Vec<i32>;
+    struct BidirectionalCircle(Vec<usize>);
 
-    impl<'a> SimpleGraph<'a, usize> for BidirectionalCircle {
+    impl SimpleGraph<'_, usize> for BidirectionalCircle {
+
+        type C = Vec<usize>;
+        
+        fn nodes(&self) -> &Self::C {
+            &self.0
+        }
 
         fn children(&self, i: usize) -> Vec::<usize> {
-            let n = self.len();
+            let n = self.0.len();
             vec![(i + n - 1) % n, (i+1) % n]
         }
     }
 
     #[test]
     fn trait_bi_circle_children_ok() {
-        let circle : BidirectionalCircle = vec![0, 1, 2, 3, 4, 5];
+        let circle = BidirectionalCircle(vec![0, 1, 2, 3, 4, 5]);
         assert_eq!(circle.children(0), vec![5, 1]);
         assert_eq!(circle.children(1), vec![0, 2]);
         assert_eq!(circle.children(2), vec![1, 3]);
@@ -160,7 +177,7 @@ mod tests {
 
     #[test]
     fn trait_bi_circle_ancestors_ok() {
-        let circle : BidirectionalCircle = vec![0, 1, 2, 3, 4, 5];
+        let circle = BidirectionalCircle(vec![0, 1, 2, 3, 4, 5]);
         assert_eq!(circle.ancestors(0), vec![1, 5]);
         assert_eq!(circle.ancestors(1), vec![0, 2]);
         assert_eq!(circle.ancestors(2), vec![1, 3]);
